09c6ed7ab412bfceadcf94805e23645589392a76
Eliam <94004944+eliamrg@users.noreply.github.com>
2023-11-24 21:01:25 -0600

    Delete Practica1.py

--- Practica1.py
+++
@@ -1,9 +0,0 @@
-import kaggle
-import os
-
-def get_csv_kaggle ():
-  print('Descargando DataSet...')
-  kaggle.api.authenticate()
-  kaggle.api.dataset_download_files('carolzhangdc/imdb-5000-movie-dataset', path=os.path.join(os.getcwd(), "dataSet"), unzip=True)
-
-get_csv_kaggle()
